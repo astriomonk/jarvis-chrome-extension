64f2614e1977988edc50595006bb646ed1af818a
astriomonk <emmettemm15@gmail.com>
2026-09-13 00:23:51 -0500

    Update content.js

--- content.js
+++ content.js
@@ -585,7 +585,28 @@ or
         message.type ===
         "TOGGLE_JARVIS"
       ) {
-        toggleJarvis();
+        function toggleJarvis() {
+
+  root.classList.toggle("open");
+
+  if (root.classList.contains("open")) {
+
+    input.focus();
+
+    if (!messages.children.length) {
+
+      const greeting =
+        "Good evening. All systems are online. How may I assist you?";
+
+      addMessage(
+        "JARVIS",
+        greeting
+      );
+
+      speak(greeting);
+    }
+  }
+}
       }
     }
   );
